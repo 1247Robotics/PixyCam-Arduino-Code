93dd637cf33cdd211fe93887339c233608c9d741
Team 1247 <1247Robotics@gmail.com>
2016-12-14 14:31:28 -0500

    Basic Hello code, unmodified

---
+++ PixyTest.ino
@@ -0,0 +1,11 @@
+//http://cmucam.org/projects/cmucam5/wiki/Hooking_up_Pixy_to_a_Microcontroller_(like_an_Arduino)
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
